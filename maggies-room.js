fcac1c436664bea01ae061eefb8bcd1b7b72b550
maggie gibson <gibsm545@newschool.edu>
2021-03-11 13:03:28 -0600

    ffff

---
+++ maggies-room.js
@@ -0,0 +1,39 @@
+console.log("maggie's room");
+
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyOvunAd4LXOhZPV'
+});
+var base = new Airtable({apiKey: 'keyOvunAd4LXOhZPV'}).base('appZs01OFTUEl5Txc');
+
+base('room-decor').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('title'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+// If you only want the first page of records, you can
+// use `firstPage` instead of `eachPage`.
+base('room-decor').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('title'));
+    });
+});
