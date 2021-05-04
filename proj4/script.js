76b52d8a5f10ceefbda2d5120ad4c26271bad13d
maggie gibson <gibsm545@newschool.edu>
2021-05-04 14:49:15 -0500

    ff

--- proj4/script.js
+++ proj4/script.js
@@ -1,1 +1,10 @@
 console.log ("hi!");
+
+var image = document.querySelectorAll("#images");
+
+
+
+image.forEach(function (picture){
+  image.style.left = `calc(${Math.random()} * (100% - 7.5rem))`;
+  image.style.top = `calc(${Math.random()} * (100% - 7.5rem))`;
+});
